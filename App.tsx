956a01532c574666369f850721c67a20f3148604
pilyeooong <pilyeooong@gmail.com>
2025-02-10 18:21:19 +0900

    add. stack, drawer navigator

--- App.tsx
+++ App.tsx
@@ -1,35 +1,13 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import AuthStackNavigator from './src/navigation/AuthStackNavigator';
+import RootNavigator from './src/navigations/root/RootNavigator';
 
 function App() {
   return (
     <NavigationContainer>
-      <AuthStackNavigator />
+      <RootNavigator />
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // margin: '10%',
-    // marginHorizontal: 10,
-    // marginVertical: 10,
-  },
-  input: {
-    flex: 1,
-    borderWidth: 2,
-    borderColor: 'black',
-    height: 100,
-    width: 100,
-  },
-  inputContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-});
 
 export default App;
---
+++ src/screens/MapHomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function MapHomeScreen() {
+  return (
+    <View>
+      <Text>Map Screen</Text>
+    </View>
+  );
+}
+
+export default MapHomeScreen;
