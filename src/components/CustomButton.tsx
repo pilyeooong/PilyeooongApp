9929ec5765df77f13d10f51e8d172c2ec5a580db
pilyeooong <pilyeooong@gmail.com>
2025-02-13 12:43:41 +0900

    add. custom button

---
+++ src/components/CustomButton.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Pressable,
+  PressableProps,
+  Dimensions,
+} from 'react-native';
+import colors from '../constants/colors';
+
+interface CustomButtonProps extends PressableProps {
+  label: string;
+  variant?: 'filled' | 'outlined';
+  size?: 'medium' | 'large';
+  inValid?: boolean;
+}
+
+// console.log(Dimensions.get('screen').height); # ios에서는 두 값이 동일
+// console.log(Dimensions.get('window').height); # android에서는 상태표시줄을 포함한 길이라 좀 다름
+const deviceHeight = Dimensions.get('screen').height;
+
+function CustomButton({
+  label,
+  variant = 'filled',
+  size = 'large',
+  inValid = false,
+  ...props
+}: CustomButtonProps) {
+  return (
+    <Pressable
+      disabled={inValid}
+      style={({ pressed }) => [
+        styles.container,
+        styles[size],
+        pressed ? styles[`${variant}Pressed`] : styles[variant],
+        inValid && styles.inValid,
+      ]}
+      {...props}>
+      <View style={styles[size]}>
+        <Text style={[styles.text, styles[`${variant}Text`]]}>{label}</Text>
+      </View>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 3,
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  inValid: {
+    opacity: 0.5,
+  },
+  filled: {
+    backgroundColor: colors.PINK_700,
+  },
+  outlined: {
+    borderColor: colors.PINK_700,
+    borderWidth: 1,
+  },
+  filledPressed: {
+    backgroundColor: colors.PINK_500,
+  },
+  outlinedPressed: {
+    borderColor: colors.PINK_700,
+    borderWidth: 1,
+    opacity: 0.5,
+  },
+  large: {
+    width: '100%',
+    paddingVertical: deviceHeight > 700 ? 15 : 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  medium: {
+    width: '50%',
+    paddingVertical: deviceHeight > 700 ? 12 : 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: '700',
+  },
+  filledText: {
+    color: colors.WHITE,
+  },
+  outlinedText: {
+    color: colors.PINK_700,
+  },
+});
+
+export default CustomButton;
--- src/screens/auth/AuthHomeScreen.tsx
+++ src/screens/auth/AuthHomeScreen.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import { Button, SafeAreaView, StyleSheet, View } from 'react-native';
+import {
+  Dimensions,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  View,
+} from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import { AuthStackParamList } from '../../navigations/stack/AuthStackNavigator';
 import { authNavigations } from '../../constants';
+import CustomButton from '../../components/CustomButton';
 
 type AuthHomeScreenProps = StackScreenProps<
   AuthStackParamList,
@@ -11,14 +18,23 @@ type AuthHomeScreenProps = StackScreenProps<
 
 function AuthHomeScreen({ navigation }: AuthHomeScreenProps) {
   return (
-    <SafeAreaView>
-      <View>
-        <Button
-          title="로그인 화면으로 이동"
+    <SafeAreaView style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image
+          resizeMode="contain"
+          style={styles.image}
+          source={require('../../assets/logo.png')}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <CustomButton
+          label="로그인"
+          variant="filled"
           onPress={() => navigation.navigate(authNavigations.LOGIN)}
         />
-        <Button
-          title="회원가입 화면으로 이동"
+        <CustomButton
+          label="회원가입"
+          variant="outlined"
           onPress={() => navigation.navigate(authNavigations.SIGNUP)}
         />
       </View>
@@ -26,6 +42,24 @@ function AuthHomeScreen({ navigation }: AuthHomeScreenProps) {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 30,
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1.5,
+    width: Dimensions.get('screen').width / 2,
+  },
+  buttonContainer: {
+    flex: 1,
+    gap: 10,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
 export default AuthHomeScreen;
